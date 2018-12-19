b748772f2527397f7ff8f477b68d9b70a23f5348
Shuo Wang <shuowang@Shuos-MacBook-Pro.local>
2018-12-19 14:41:17 -0500

    add the feature of search images from giphy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ImageFlow from './ImageFlow';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      imageAddrs: [],
+    };
+    this.timeout = null;
+  }
+
+  handleUpdateSearchText = event => {
+    this.setState({
+      searchText: event.target.value,
+    });
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(searchText => {
+      axios.get(`https://api.giphy.com/v1/gifs/search?api_key=YWvK8etOQgpDuzevIRng2Np91aSn38PF&q=${searchText}`)
+      .then(res => res.data.data)
+      .then(res => res.map(data => data.images.original.url))
+      .then(addr => {
+        this.setState({
+          imageAddrs: addr,
+        });
+      });
+    }, 1000, event.target.value);
+  }
+  
   render() {
+    const { searchText, imageAddrs } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" value={searchText} onChange={this.handleUpdateSearchText}/>
+        <ImageFlow imageAddrs={imageAddrs} />
       </div>
     );
   }
